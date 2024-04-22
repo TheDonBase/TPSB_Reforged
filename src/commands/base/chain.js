a90b615c853de71544e89376bc5a78b9fa88def5
TheDonBase <basebulldozer@gmail.com>
2024-04-23 00:46:34 +0200

    fixed typo

--- src/commands/base/chain.js
+++ src/commands/base/chain.js
@@ -14,35 +14,35 @@ module.exports = {
         const id = interaction.options.getInteger('id');
         const url = `https://croaztek.com/admin/api_get_chain/${id}`;
         try {
-                    const response = await fetch(url, {
-                        method: 'GET'
-                    });
-                    if (!response.ok) {
-                        interaction.reply("There was an error getting the chain details, please contact TheDonBase");
-                    }
-                    const chain = await response.json();
-        
-                    // Constructing the embed
-                    const embed = new MessageEmbed()
+            const response = await fetch(url, {
+                method: 'GET'
+            });
+            if (!response.ok) {
+                interaction.reply("There was an error getting the chain details, please contact TheDonBase");
+            }
+            const chain = await response.json();
+
+            // Constructing the embed
+            const embed = new MessageEmbed()
                         .setColor('#0099ff')
                         .setTitle('Chain Details')
                         .addField('Chain ID', chain.id)
                         .addField('Date', chain.date.date);
-        
-                    // Adding days and guards information
-                    chain.days.forEach(day => {
-                        const guardsString = chain.guards
+
+            // Adding days and guards information
+            chain.days.forEach(day => {
+                const guardsString = chain.guards
                             .filter(guard => guard.day === day.date)
                             .map(guard => `${guard.hour}: ${guard.player}`)
                             .join('\n');
-                        embed.addField(day.day, `${day.date}\nGuards:\n${guardsString}`);
-                    });
-        
-                    // Sending the embed
-                    await interaction.reply({ embeds: [embed] });
-                } catch (error) {
-                    console.error('Error fetching or processing data:', error);
-                    await interaction.reply('Failed to fetch or process data.');
-                }
-            },
-}
+                embed.addField(day.day, `${day.date}\nGuards:\n${guardsString}`);
+            });
+
+            // Sending the embed
+            await interaction.reply({ embeds: [embed] });
+        } catch (error) {
+            console.error('Error fetching or processing data:', error);
+            await interaction.reply('Failed to fetch or process data.');
+        }
+    },
+};
