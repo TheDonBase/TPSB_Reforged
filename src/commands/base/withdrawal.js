7510e416e4edc3f9ae80e76f503e2428d3cb92f9
TheDonBase <basebulldozer@gmail.com>
2024-04-29 17:09:13 +0200

    Fixed some issues

--- src/commands/base/withdrawal.js
+++ src/commands/base/withdrawal.js
@@ -1,36 +1,42 @@
 const { SlashCommandBuilder, userMention } = require('discord.js');
 const Logger = require('../../utils/logger');
 
-
 module.exports = {
     data: new SlashCommandBuilder()
-	.setName('withdrawal')
-    .setDescription('Notify bankers that you need a withdrawal! You can either type \"All\" Or an actual number')
-    .addStringOption(option =>
-        option
-        .setName('amount')  // Ensure the name is set to 'Amount'
-        .setDescription('Choose your amount')
-        .setRequired(true)
+        .setName('withdrawal')
+        .setDescription('Notify bankers that you need a withdrawal. You can either type "All" or an actual number.')
+        .addStringOption(option =>
+            option
+                .setName('amount')  // Ensure the name is set to 'Amount'
+                .setDescription('Choose your amount')
+                .setRequired(true)
         ),
     async execute(interaction) {
         try {
-            const banker_role = '<@&1165659166501785741>';
-            const banker_channel = interaction.guild.channels.cache.get('815742620802089000');
-            const amountOption = interaction.options.getString('amount');
-            if (isNaN(amountOption) && amountOption.toLocaleLowerCase() === 'all') {
-                await banker_channel.send(`${banker_role} ${userMention(interaction.user.id)} Wants to withdraw **All** Of his cash from the faction bank!`);
-                interaction.reply("I have notified the bankers :)")
-            } else if (!isNaN(amountOption) && amountOption >= 0) {
-                await banker_channel.send(`${banker_role} ${userMention(interaction.user.id)} Wants to withdraw **$${formatNumberWithCommas(amountOption)}** Of their cash from the faction bank!`);
-                interaction.reply("I have notified the bankers :)")
+            const bankerRoleId = '1165659166501785741'; // Update with actual role ID
+            const bankerChannelId = '815742620802089000'; // Update with actual channel ID
+
+            const bankerRoleMention = `<@&${bankerRoleId}>`;
+            const bankerChannel = interaction.guild.channels.cache.get(bankerChannelId);
+
+            const amountOption = interaction.options.getString('amount').toLowerCase();
+
+            if (isNaN(amountOption) && amountOption === 'all') {
+                await bankerChannel.send(`${bankerRoleMention} ${userMention(interaction.user.id)} wants to withdraw **All** of their cash from the faction bank!`);
+            } else if (!isNaN(amountOption) && parseFloat(amountOption) >= 0) {
+                await bankerChannel.send(`${bankerRoleMention} ${userMention(interaction.user.id)} wants to withdraw **$${formatNumberWithCommas(amountOption)}** of their cash from the faction bank!`);
+            } else if (!isNaN(amountOption) && parseFloat(amountOption) === 0) {
+                await interaction.reply("You seem to want to withdraw ***0***$. Is that correct?");
+                return;
+            } else {
+                await interaction.reply("Invalid input. Please provide either 'All' or a valid amount.");
+                return;
             }
-            if(!isNaN(amountOption) && amountOption === 0) {
-                interaction.reply("You seem to be wanting to withdraw ***0***$ is that really right?")
-            }
-            interaction.reply("Done :)")
+
+            await interaction.reply("I have notified the bankers :)");
         } catch (error) {
             Logger.error(`There was an error executing ${this.data.name} with error: ${error}`);
-            interaction.reply("There was an unexpected error :( please contact TheDonBase")
+            interaction.reply("There was an unexpected error. Please contact TheDonBase.");
         }
     },
 };
