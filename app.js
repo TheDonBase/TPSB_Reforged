3a2735d31ffd7c351c7f1cfb697369d5933ba838
TheDonBase <basebulldozer@gmail.com>
2024-10-02 16:10:37 +0200

    Adding public folder

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ const app = express();
 const port = 8080;
 
 app.use(express.json());
+app.use(express.static("public"))
 
 function setupRoutesForPublicDir(directory, app) {
     fs.readdirSync(directory).forEach((file) => {
